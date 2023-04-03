1355831a89a911530ff7cfd15e3c4bd1ad3d2c59
daoninhthai <thaimeo1131@gmail.com>
2023-04-03 18:35:28 +0700

    them test cho service moi

--- src/components/common/ErrorBoundary74.js
+++ src/components/common/ErrorBoundary74.js
@@ -66,3 +66,19 @@ class ErrorBoundary74 extends Component {
 }
 
 export default ErrorBoundary74;
+
+
+/**
+ * Debounce function to limit rapid invocations.
+ * @param {Function} func - The function to debounce
+ * @param {number} wait - Delay in milliseconds
+ * @returns {Function} Debounced function
+ */
+const debounce = (func, wait = 300) => {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+};
+
